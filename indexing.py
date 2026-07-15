998229ce2a7b151e2da724977a458268b50f6d12
Dhruv220605 <143691219+Dhruv220605@users.noreply.github.com>
2026-07-15 21:19:09 +0530

    add indexing

---
+++ indexing.py
@@ -0,0 +1,14 @@
+#slicing 
+
+n="1234-5678-9012"
+
+print(n[4])
+print(n[:4])
+print(n[2:6])
+print(n[1:9:2])
+print(n[-1])
+print(n[::2])
+
+
+credit = "1212 3434 5656"
+print(f"credit no. is : XXXX XXXX {credit[-4:]}")
